b6366c99a4e702331d756a06fc9a7c749e8800cf
Selva <selvaraj.arumuthu@gmail.com>
2019-04-19 20:35:21 -0400

    unit test example

---
+++ test/com/selva/example/MainTest.java
@@ -0,0 +1,19 @@
+package com.selva.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
